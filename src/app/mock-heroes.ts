74358f83c45af33d56f85f2b2024d0b8db3ccb13
Delsy Noelia Cuellar Chung <166728764+noeliacuellar3@users.noreply.github.com>
2024-05-02 22:11:47 -0400

    Add files via upload

---
+++ src/app/mock-heroes.ts
@@ -0,0 +1,13 @@
+import { Hero } from './hero';
+//definicion y exportación del array heroes 
+export const HEROES: Hero[] = [
+  { id: 12, name: 'Bolivia' },
+  { id: 13, name: 'Brasil' },
+  { id: 14, name: 'Colombia' },
+  { id: 15, name: 'Paraguay' },
+  { id: 16, name: 'México' },
+  { id: 17, name: 'Argentina' },
+  { id: 18, name: 'Ecuador' },
+  { id: 19, name: 'Perú' },
+  { id: 20, name: 'Venezuela' }
+];
